713df947b645eeb15850866c8df4e12c2db8ccad
Illia <ilya.kvitkovskyi@gmail.com>
2021-11-16 20:31:04 +0200

    added full shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,22 @@
-$(function() {
+$(function () {
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+  });
+
+  $('.select-style').styler();
 
   $('.filter-price__input').ionRangeSlider({
-    type: "double",
-    prefix: "$",
+    type: 'double',
+    prefix: '$',
     onStart: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
@@ -18,13 +32,13 @@ $(function() {
     arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
   });
 
-  $(".star").rateYo({
-    starWidth: "17px",
-    normalFill: "#ccccce",
-    ratedFill: "#ffc35b",
+  $('.star').rateYo({
+    starWidth: '17px',
+    normalFill: '#ccccce',
+    ratedFill: '#ffc35b',
     readOnly: true,
   });
 
@@ -34,41 +48,40 @@ $(function() {
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
+
     return {
       total,
       days,
       hours,
       minutes,
-      seconds
+      seconds,
     };
   }
-  
+
   function initializeClock(id, endtime) {
     const clock = document.querySelector('.promo__clock');
     const daysSpan = clock.querySelector('.promo__days');
     const hoursSpan = clock.querySelector('.promo__hours');
     const minutesSpan = clock.querySelector('.promo__minutes');
     const secondsSpan = clock.querySelector('.promo__seconds');
-  
+
     function updateClock() {
       const t = getTimeRemaining(endtime);
-  
+
       daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
     }
-  
+
     updateClock();
     const timeinterval = setInterval(updateClock, 1000);
   }
-  
+
   const deadline = $('.promo__clock').attr('data-time');
   initializeClock('promo__clock', deadline);
-
 });
